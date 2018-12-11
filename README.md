9c6cfd101bf74a4703779a3c24e8d9f37a852268
petermcneil <petermcneil@users.noreply.github.com>
2018-12-11 20:26:07 +0000

    Added invalidation to aws-setup.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-PETER MCNEIL Website
+Popski
+---
+A personal website written by Peter McNeil. Using Jekyll as the static generator
+ with S3 as hosting.
 
+CSS/HTML/JS are all painstakingly written by myself through self-teaching.
+
+#### Scripts
+`aws-update` is a script that will upload the statically generated website to S3 and 
+invalidate cloudfront with a flag `-i`. Needs cleaning up.
--- aws-update.py
+++ aws-update.py
@@ -1,15 +1,16 @@
 #!/usr/local/bin/python3
-import boto3.session
+import argparse
+import configparser
+import datetime
+import gzip
 import hashlib
 import os
-import time
-import configparser
-import gzip
 import shutil
+import subprocess
+import time
 from time import gmtime, strftime
 
-aws = boto3.session.Session(profile_name='popski')
-s3 = aws.resource('s3')
+import boto3.session
 
 this_file = os.path.abspath(os.path.dirname(__file__))
 config = configparser.ConfigParser()
@@ -23,7 +24,10 @@ backup_bucket_name = popski["backup_bucket_name"]
 built_website = popski["static_website"]
 temp_folder = popski["temp_folder"]
 
-excluded = [".DS_Store", "function.ts", "feed.xml"]
+excluded = [".DS_Store", "function.ts", "feed.xml", ".sass-cache", ".scssc", ".scss"]
+
+excluded_ext = [".scssc", ".md"]
+pattern = '%d.%m.%Y %H:%M:%S'
 
 mime_type = {
     "html": "text/html",
@@ -38,14 +42,19 @@ mime_type = {
     "webp": "image/webp"
 }
 
+AWS = boto3.session.Session(profile_name='popski')
+S3_CLIENT = AWS.client("s3")
+S3 = AWS.resource('s3')
+CLOUDFRONT = AWS.client("cloudfront")
+main_bucket = S3.Bucket(main_bucket_name)
+
 
 def backup_website():
-    main_bucket = s3.Bucket(main_bucket_name)
     backup_path = strftime("%Y-%m-%d/%H:%M:%S/", gmtime())
 
     for item in main_bucket.objects.all():
         print("Backing up file: {}".format(item.key))
-        s3.meta.client.copy_object(
+        S3.meta.client.copy_object(
             ACL='public-read',
             Bucket=backup_bucket_name,
             CopySource={'Bucket': main_bucket_name, 'Key': item.key},
@@ -76,8 +85,6 @@ def gzip_files():
 
 
 def load_to_s3():
-    main_bucket = s3.Bucket(main_bucket_name)
-
     print("Deleting contents of the bucket {}".format(main_bucket_name))
     main_bucket.objects.all().delete()
 
@@ -95,24 +102,62 @@ def load_to_s3():
                 data = open(file_path, "rb")
                 content_type = find_content_type(file_path)
 
-                if "text/html" not in content_type: 
-                    main_bucket.put_object(Bucket=main_bucket_name, Key=key, Body=data, 
-                        ContentType=content_type, ContentEncoding="gzip", ACL="public-read")
+                if "text/html" not in content_type:
+                    main_bucket.put_object(Bucket=main_bucket_name, Key=key, Body=data,
+                                           ContentType=content_type, ContentEncoding="gzip", ACL="public-read")
                 else:
-                    main_bucket.put_object(Bucket=main_bucket_name, Key=key, Body=data, 
-                        ContentType=content_type, ContentEncoding="gzip", ACL="public-read", CacheControl="max-age=3600")
+                    main_bucket.put_object(Bucket=main_bucket_name, Key=key, Body=data,
+                                           ContentType=content_type, ContentEncoding="gzip", ACL="public-read", CacheControl="max-age=3600")
 
 
-def invalidate_cloudfront():
+def update_cloudfront():
     print("Updating Cloudfront distribution with new index path")
-    client = aws.client("cloudfront")
-    dist_config = client.get_distribution_config(Id=CLOUDFRONT_ID)
+    dist_config = CLOUDFRONT.get_distribution_config(Id=CLOUDFRONT_ID)
 
     dist_config["DistributionConfig"]["DefaultRootObject"] = index_html
 
     dis_config = dist_config["DistributionConfig"]
     etag = dist_config["ETag"]
-    client.update_distribution(DistributionConfig=dis_config, Id=CLOUDFRONT_ID, IfMatch=etag)
+    CLOUDFRONT.update_distribution(DistributionConfig=dis_config, Id=CLOUDFRONT_ID, IfMatch=etag)
+
+
+def files_to_invalidate():
+    invalidation = []
+    git_command_1 = "git ls-files --full-name"
+    git_command_2 = "grep \"$(git diff --name-only HEAD)\""
+    p1 = subprocess.Popen(git_command_1, stdout=subprocess.PIPE, shell=True)
+    p2 = subprocess.Popen(git_command_2, stdin=p1.stdout, stdout=subprocess.PIPE, shell=True)
+    p1.stdout.close()
+    process, _ = p2.communicate()
+
+    process = process.decode("utf-8").strip()
+
+    for file in process.split("\n"):
+        ext = file.split(".")[1]
+        if "website" in file and ext not in excluded_ext and file != "website/index.html":
+            file = file.replace("website/", "")
+
+            if ".scss" in file:
+                file = file.replace(".scss", ".css")
+
+            invalidation.append(file)
+
+    return invalidation
+
+
+def invalidate_cloudfront(l):
+    if len(l) > 0:
+        print("Invalidating the following paths: {}".format(l))
+        CLOUDFRONT.create_invalidation(
+            DistributionId=CLOUDFRONT_ID,
+            InvalidationBatch={
+                'Paths': {
+                    'Quantity': len(l),
+                    'Items': ['/{}'.format(f) for f in l]
+                },
+                'CallerReference': 'website-updated-{}'.format(datetime.datetime.now())
+            }
+        )
 
 
 def md5(files):
@@ -131,15 +176,27 @@ def md5(files):
     print("Setting index filename to {}".format(index_html))
 
 
-def main():
+def main(argsv):
     backup_website()
     md5(["index.html"])
     gzip_files()
     load_to_s3()
-    invalidate_cloudfront()
+    update_cloudfront()
 
     shutil.rmtree(temp_folder)
 
+    if argsv.i:
+        i_list = files_to_invalidate()
+
+        if argsv.f:
+            i_list = "/*"
+        invalidate_cloudfront(i_list)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='Force invalidation of all paths', required=False)
+    parser.add_argument('-i', help='Invalidate all updated objects', required=False, action='store_true')
+    args = parser.parse_args()
+
+    main(args)

6a17e56922d64a65761f9c7634626c52fe267f23
petermcneil <petermcneil@users.noreply.github.com>
2019-06-24 20:57:29 +0100

    Updated the way projects work

--- _site/assets/scripts/main.js
+++ _site/assets/scripts/main.js
@@ -1,1 +1,79 @@
-function changeMode(){modeToggle.checked=document.documentElement.style.getPropertyValue("--background").includes(darkMode.backgroundColour),toggleMode(modeToggle.checked)}function toggleMode(e){let o=!0;"string"==typeof e&&(e=e===lightMode.mode||""===e,o=!1),setMode(e?lightMode:darkMode,o)}function setMode(e,o){document.documentElement.style.setProperty("--text",e.textColour),document.documentElement.style.setProperty("--links",e.linkColour),document.documentElement.style.setProperty("--linkHover",e.linkHover),document.documentElement.style.setProperty("--background",e.backgroundColour),themeColour.setAttribute("content",e.backgroundColour),msColour.setAttribute("content",e.backgroundColour),appleColour.setAttribute("content","default"),o&&setCookie(e.mode),modeToggle.checked=e.checked}function setCookie(e){let o=window.location.hostname,t=new Date;t.setMonth(t.getMonth()+12),document.cookie="mode="+e+";expires="+t+"; path=/; secure;domain=."+o}function getCookie(e){let o,t;return document.cookie.length>0&&-1!==(o=document.cookie.indexOf(e+"="))?(o=o+e.length+1,-1===(t=document.cookie.indexOf(";",o))&&(t=document.cookie.length),unescape(document.cookie.substring(o,t))):""}class CSSMode{constructor(e,o,t,n,l,c){this.mode=e,this.backgroundColour=o,this.textColour=t,this.linkColour=n,this.linkHover=l,this.checked=c}}const modeToggle=document.getElementById("mode__toggle"),themeColour=document.querySelector('meta[name="theme-color"]'),msColour=document.querySelector('meta[name="msapplication-navbutton-color"]'),appleColour=document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]'),darkMode=new CSSMode("dark","black","white","white","gray",!0),lightMode=new CSSMode("light","white","black","blue","#0e64ef",!1);toggleMode(getCookie("mode"));
+class CSSMode {
+    constructor(mode, background, text, links, linkHover, checked) {
+        this.mode = mode;
+        this.backgroundColour = background;
+        this.textColour = text;
+        this.linkColour = links;
+        this.linkHover = linkHover;
+        this.checked = checked;
+    }
+}
+
+const modeToggle = document.getElementById("mode__toggle");
+
+const themeColour = document.querySelector('meta[name="theme-color"]');
+const msColour = document.querySelector('meta[name="msapplication-navbutton-color"]');
+const appleColour = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
+
+const darkMode = new CSSMode("dark", "black", "white", "white", "gray",true);
+const lightMode = new CSSMode("light", "white", "black", "blue", "#0e64ef",false);
+
+function changeMode() {
+    modeToggle.checked = document.documentElement
+        .style
+        .getPropertyValue('--background')
+        .includes(darkMode.backgroundColour);
+
+    toggleMode(modeToggle.checked);
+}
+
+function toggleMode(changeToLightMode) {
+    let setCookie = true;
+    if (typeof changeToLightMode === "string") {
+        changeToLightMode = changeToLightMode === lightMode.mode || changeToLightMode === "";
+        setCookie = false;
+    }
+
+    setMode(changeToLightMode ? lightMode : darkMode, setCookie);
+}
+
+function setMode(mode, setCookieB) {
+    document.documentElement.style.setProperty('--text', mode.textColour);
+    document.documentElement.style.setProperty('--links', mode.linkColour);
+    document.documentElement.style.setProperty('--linkHover', mode.linkHover);
+
+    document.documentElement.style.setProperty('--background', mode.backgroundColour);
+
+    themeColour.setAttribute("content", mode.backgroundColour);
+    msColour.setAttribute("content", mode.backgroundColour);
+    appleColour.setAttribute("content", "default");
+
+    if (setCookieB) {
+        setCookie(mode.mode);
+    }
+    modeToggle.checked = mode.checked;
+}
+
+function setCookie(mode) {
+    let domain = window.location.hostname;
+    let date = new Date();
+    date.setMonth(date.getMonth() + 12);
+    document.cookie = "mode=" + mode + ";expires=" + date + "; path=/; secure;domain=." + domain;
+}
+
+function getCookie(c_name) {
+    let c_start, c_end;
+
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start !== -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end === -1) c_end = document.cookie.length;
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+toggleMode(getCookie("mode"));
